556934f55fcb84f26b3cb0267f1b396e5792f84b
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-29 04:48:48 +0530

    controls

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import {FirstPersonControls} from 'three/examples/jsm/controls/FirstPersonControls'
+import {FlyControls} from 'three/examples/jsm/controls/FlyControls'
+import {TransformControls} from 'three/examples/jsm/controls/TransformControls'
 const scene=new THREE.Scene();
-scene.background=new THREE.Color(0.5,0.5,0.5)
+// scene.background=new THREE.Color(0.5,0.5,0.5)
 const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.z=50;
+camera.position.z=10;
+camera.position.y=5;
 // renderer using canvas
 const renderer=new THREE.WebGLRenderer();
 // size for the canvas
@@ -12,113 +16,64 @@ renderer.setSize(window.innerWidth,window.innerHeight)
 document.getElementById('app').appendChild(renderer.domElement)
 
 // lights
-const ambianceLight=new THREE.AmbientLight(0xffffff,0.3)
+const ambianceLight=new THREE.AmbientLight(0xffffff,0.2)
 // arguments are color and intensity
 scene.add(ambianceLight)
 
 const pointLight=new THREE.PointLight(0xffffff,1,100)
 // arguments are color intensity and available in distance in units 
-// pointLight.position.set(5,0,1)
+pointLight.position.set(0,5,5)
 scene.add(pointLight)
 
 // to create a shape
-// const geometry=new THREE.BoxGeometry(5,5,5)
-// const geometry=new THREE.SphereGeometry(5,32,32)
-// capsule geometry
-// const geometry=new THREE.CapsuleGeometry(1,1,50,8)
-// const geometry=new THREE.CircleGeometry(2,32)
-// const geometry=new THREE.ConeGeometry(2,2,50)
-// const geometry=new THREE.CylinderGeometry(5,5,32,16);
-
-// const geometry=new THREE.BufferGeometry()
-// const vertices=new Float32Array([
-//     -1.0,-1.0,1.0,
-//     1.0,-1.0,1.0,
-//     1.0,1.0,1.0,
-
-//     1.0,1.0,1.0,
-//     -1.0,1.0,1.0,
-//     -1.0,-1.0,1.0,
-// ]);
-// geometry.setAttribute('position',new THREE.BufferAttribute(vertices,3))
-
-const geometry=new THREE.BufferGeometry();
-const vertices=new Float32Array([
-    -1.0,-1.0,1.0,
-    1.0,-1.0,1.0,
-    1.0,1.0,1.0,
-    -1.0,1.0,1.0,
-]);
-const color=new Float32Array([
-    1.0,0.0,0.0,
-    0.0,1.0,0.0,
-    1.0,1.0,1.0
-]);
-const normals=new Float32Array([
-    1.0,1.0,1.0,
-    1.0,1.0,1.0,
-    1.0,1.0,1.0,
-    1.0,1.0,1.0,
-])
-const indeces=[
-    0,1,2,
-    2,3,0
-]
-geometry.setAttribute('position',new THREE.BufferAttribute(vertices,3))
-geometry.setAttribute('color',new THREE.BufferAttribute(color,3))
-geometry.setAttribute('normal',new THREE.BufferAttribute(normals,3))
-geometry.setIndex(indeces)
-
-
-const material=new THREE.MeshStandardMaterial({vertexColors:true})
-const mesh=new THREE.Mesh(geometry,material);
+const geometry=new THREE.BoxGeometry(1,1,1)
+const material=new THREE.MeshStandardMaterial()
+const mesh=new THREE.Mesh(geometry,material)
+mesh.position.set(0,0,0)
 scene.add(mesh)
 
-// const material=new THREE.MeshBasicMaterial({color:'red'})
-// if we want it to react on light the we have to use 
-// const material=new THREE.MeshStandardMaterial({color:'red'})
-// const cube=new THREE.Mesh(geometry,material);
-// const material=new THREE.PointsMaterial({color:'red',size:0.5})
-// const cube=new THREE.Points(geometry,material);
 
-// if using Line to see in skeleton form
-// const cube=new THREE.Line(geometry,material);
 
-const lightGeometry=new THREE.SphereGeometry(1,12,16)
-const lightmaterial=new THREE.MeshBasicMaterial({color:'white'})
-const lightSphare=new THREE.Mesh(lightGeometry,lightmaterial)
 
-// scene.add(cube,lightSphare);
-scene.add(lightSphare);
 
-pointLight.position.set(0,0,20)
-     
-    lightSphare.position.set(0,0,20)
+
 const controls=new OrbitControls(camera,renderer.domElement)
+
+const gridHelper=new THREE.GridHelper(100,10)
+scene.add(gridHelper)
+const clock=new THREE.Clock()
+// const controls=new FirstPersonControls(camera,renderer.domElement)
+// controls.movementSpeed=150;
+// controls.lookSpeed=0.1
+
+// const controls=new FlyControls(camera,renderer.domElement)
+// controls.movementSpeed=1000;
+// controls.rollSpeed=Math.PI /24;
+ const transformControl=new TransformControls(camera,renderer.domElement)
+transformControl.addEventListener('change',animate)
+transformControl.addEventListener('dragging-changed',function(event){
+    controls.enabled=!event.value
+})
+
+transformControl.attach(mesh)
+scene.add(transformControl)
+
+
+
 
 
 let flag=true;
 
 
-let q=0
+
 animate()
 function animate()
 {
-    controls.update();
-    q+=0.01
-    let qsin=Math.sin(q);
-    let qcos=Math.cos(q);
-    // cube.position.x=3*qsin
+    // controls.update();
+    controls.update(clock.getDelta())
+    
 
-    let scaledCos=30 *qcos
-    let scaledSin=30 *qsin
-    pointLight.position.set(scaledCos,0,scaledSin)
-     
-    lightSphare.position.set(scaledCos,0,scaledSin)
-
-    // cube.rotation.x+=0.01
-    //     cube.rotation.y+=0.01
-    //     cube.rotation.z+=0.01
+    
     renderer.render(scene,camera)
     requestAnimationFrame(animate);
 }
